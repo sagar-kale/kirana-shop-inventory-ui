3ae6abfb002fe59c66e0d9942ec951087a9086a1
sagar <sagark.kale@hotmail.com>
2019-12-08 02:45:23 +0530

    progress bar added

--- src/app/layout/bs-component/components/progressbar/progressbar.component.ts
+++ src/app/layout/bs-component/components/progressbar/progressbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-progressbar',
-  templateUrl: './progressbar.component.html',
-  styleUrls: ['./progressbar.component.scss']
+    selector: 'app-progressbar',
+    templateUrl: './progressbar.component.html',
+    styleUrls: ['./progressbar.component.scss']
 })
 export class ProgressbarComponent implements OnInit {
+    progress: Number = 0;
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    startProgress(): void {
+        setTimeout(() => {
+            for (let i = 0; i <= 100; i++) {
+                this.progress = i;
+            }
+        }, 100);
+    }
 }
