9ca3e143460a2669782613ddc43b86f1e56bbb1d
sagar <sagark.kale@hotmail.com>
2019-12-13 01:24:52 +0530

    feature : update product functionality added

--- src/app/layout/product/product.component.ts
+++ src/app/layout/product/product.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
-import { ProductService } from './product.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { SharedService } from 'src/app/shared/services/shared.service';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { SharedService } from 'src/app/shared/services/shared.service';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'app-product',
@@ -23,7 +24,11 @@ export class ProductComponent implements OnInit, AfterViewInit {
     productName$: Subject<string> = new Subject();
     nameExists = false;
     closeResult = '';
-    product: any;
+    productIdUpdate = null;
+    btnSaveUpdateTitle = 'save';
+    pTabTitle = 'Add Product';
+    btnSavingUpdatingTitle = 'Saving Product';
+    isNew = true;
 
     constructor(
         private productService: ProductService,
@@ -51,45 +56,66 @@ export class ProductComponent implements OnInit, AfterViewInit {
             .pipe(debounceTime(500), distinctUntilChanged())
             .subscribe(value => this.checkName(value));
     }
-    saveProduct() {
-        this.isLoading = true;
+    onSubmit() {
         this.submitted = true;
-        this.errorsMap = null;
+        this.isLoading = true;
+        if (this.productForm.invalid) {
+            return;
+        }
+        this.isNew = true;
+        this.saveProduct(this.productForm.value);
+    }
 
-        this.productService.saveProduct(this.productForm.value).subscribe(
-            res => {
-                this.isLoading = false;
-                if (res.error) {
-                    this.sharedService.displayError(res.errMessage);
-                    return;
-                } else if (res.validationError) {
-                    this.errorsObj = res.validationErrors;
-                    this.errorsMap = this.errorsObj;
-                    for (const key of Object.keys(res.validationErrors)) {
-                        this.productForm.controls[key].setErrors({
-                            incorrect: true
-                        });
+    saveProduct(product: Product) {
+        this.errorsMap = null;
+        if (this.productIdUpdate === null) {
+            this.productService.saveProduct(product).subscribe(
+                res => {
+                    this.clearForm();
+                    if (res.error) {
+                        this.sharedService.displayError(res.errMessage);
+                        return;
+                    } else if (res.validationError) {
+                        this.errorsObj = res.validationErrors;
+                        this.errorsMap = this.errorsObj;
+                        for (const key of Object.keys(res.validationErrors)) {
+                            this.productForm.controls[key].setErrors({
+                                incorrect: true
+                            });
+                        }
+                        return;
                     }
-                    return;
+                    this.sharedService.displaySuccessMsg(res.msg);
+                    this.productForm.reset();
+                },
+                err => {
+                    this.clearForm();
+                    console.log(err);
+                    if (err.status === 0) {
+                        this.sharedService.buildHtmlAlert(
+                            'Error',
+                            `Please check spring boot application is runnning or not <br><small> ${err.message}</small>`,
+                            'error'
+                        );
+                        return;
+                    }
+                    this.sharedService.buildHtmlAlert('Error', err, 'error');
                 }
-                this.sharedService.displaySuccessMsg(res.msg);
-                this.productForm.reset();
-            },
-            err => {
-                this.isLoading = false;
-                console.log(err);
-                if (err.status === 0) {
-                    this.sharedService.buildHtmlAlert(
-                        'Error',
-                        `Please check spring boot application is runnning or not <br><small> ${err.message}</small>`,
-                        'error'
-                    );
-                    return;
-                }
-                this.sharedService.buildHtmlAlert('Error', err, 'error');
-            }
-        );
-        this.submitted = false;
+            );
+        } else {
+            console.log('updating existing product...', this.productIdUpdate);
+            console.log(product);
+            product.id = this.productIdUpdate;
+            setTimeout(() => {
+                this.sharedService.buildAlert(
+                    'Update',
+                    JSON.stringify(product),
+                    'success'
+                );
+                this.productIdUpdate = null;
+                this.clearForm();
+            }, 5000);
+        }
     }
 
     updateProduct(formValue: any): void {
@@ -127,7 +153,7 @@ export class ProductComponent implements OnInit, AfterViewInit {
     }
 
     open(content: any, productId: number) {
-        this.getProduct(productId);
+        // this.getProduct(productId);
         this.modalService.open(content, { size: 'lg' }).result.then(
             result => {
                 this.closeResult = `Closed with: ${result}`;
@@ -148,12 +174,26 @@ export class ProductComponent implements OnInit, AfterViewInit {
         }
     }
 
-    getProduct(id: number): void {
+    loadProductToEdit(id: number): void {
         this.productService.retriveProductById(id).subscribe(
             res => {
-                this.product = res;
+                this.isNew = false;
+                this.btnSaveUpdateTitle = 'Update';
+                this.pTabTitle = 'Update Product';
+                this.btnSavingUpdatingTitle = 'Updating Product';
+                this.productIdUpdate = res.id;
+                this.productForm.patchValue(res);
             },
             err => this.sharedService.buildAlert('Error', err, 'error')
         );
+    }
+    clearForm() {
+        this.submitted = false;
+        this.isNew = true;
+        this.isLoading = false;
+        this.btnSaveUpdateTitle = 'save';
+        this.pTabTitle = 'Add Product';
+        this.btnSavingUpdatingTitle = 'Saving Product';
+        this.productForm.reset();
     }
 }
--- src/app/layout/product/product.ts
+++ src/app/layout/product/product.ts
@@ -1,2 +1,10 @@
 export class Product {
+    id: number;
+    productName: string;
+    catId: number;
+    qty: number;
+    measure: string;
+    purchasePrice: number;
+    salePrice: number;
+    desc: string;
 }
